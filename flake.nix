c0a0f71b4c411c1d62f50095da31f9315b62ab25
Sleroq <hireme@sleroq.link>
2024-07-05 22:21:24 +0300

    Updated flake
    Added docker image and default package

--- flake.nix
+++ flake.nix
@@ -1,32 +1,32 @@
-{
-  inputs = {
+{ inputs = {
     templ = {
       type = "git";
       url = "https://github.com/a-h/templ";
       ref = "refs/tags/v0.2.707";
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = inputs@{ nixpkgs, ... }:
-  let
-    allSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-    forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-      inherit system;
-      pkgs = import nixpkgs { inherit system; };
-    });
-    templ = system: inputs.templ.packages.${system}.templ;
-  in {
-    devShell = forAllSystems ({ pkgs, system }:
-      pkgs.mkShell {
-        buildInputs = with pkgs; [
-          go
-          (templ system)
-        ];
-    });
-  };
+  outputs = inputs@{ self, nixpkgs, flake-utils, templ }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = import nixpkgs { inherit system; };
+        matrix-wiki = pkgs.callPackage ./package.nix { };
+        docker-container = pkgs.dockerTools.buildImage {
+          name = "matrix-wiki-docker";
+          config = {
+            Cmd = [ "${matrix-wiki}/bin/matrix-wiki" ];
+          };
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            templ.packages.${system}.templ
+          ];
+        };
+        packages.default = matrix-wiki;
+        packages.docker = docker-container;
+      }
+    );
 }
